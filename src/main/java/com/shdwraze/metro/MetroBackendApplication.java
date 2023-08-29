07d45803d129391aa2f425033e2ce5677943332e
Jaroslava <shdwrazedev@gmail.com>
2023-08-29 17:31:14 +0300

    Create project

---
+++ src/main/java/com/shdwraze/metro/MetroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shdwraze.metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shdwraze/metro/MetroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shdwraze.metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
