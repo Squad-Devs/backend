1f6a8a4af448ce0aee05c4af73d87f6c7db3978a
Jaroslava <shdwrazedev@gmail.com>
2023-09-14 18:09:33 +0300

    changes: config for deployment

--- src/main/java/com/shdwraze/metro/config/FirestoreConfig.java
+++ src/main/java/com/shdwraze/metro/config/FirestoreConfig.java
@@ -3,24 +3,22 @@ package com.shdwraze.metro.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 
 @Configuration
 public class FirestoreConfig {
 
+    @Value("${firestore.key}")
+    private String firestoreKey;
+
     @Bean
     public Firestore getFirestore() throws IOException {
-        ClassLoader classLoader = this.getClass().getClassLoader();
-
-        InputStream serviceAccount = new FileInputStream(
-                Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile()
-        );
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firestoreKey.getBytes());
         GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
         FirestoreOptions firestoreOptions =
                 FirestoreOptions.getDefaultInstance().toBuilder()
